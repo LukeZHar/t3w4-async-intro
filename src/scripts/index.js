7fe0b658e0ae02c6d8c1a72d3be6c37ff8c9abe0
LukeZHar <15649@coderacademy.edu.au>
2024-11-06 08:46:31 +1000

    Feat: Project init

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+console.log("This file is running")
+
+let defaultEnv = {
+    port: 5050,
+    DATABASE_URL: "ASDSA",
+    JWT_SECURITY_KEY: "ASDSA",
+    SECRET_KEY: "apple",
+    SECURE_API_KEY: "ASDSA"
+}
