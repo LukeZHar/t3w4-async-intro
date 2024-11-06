c51acfdfd66d66d2f0fda142679b9bbe87aa4a4c
LukeZHar <15649@coderacademy.edu.au>
2024-11-06 10:57:50 +1000

    Feat: Error handling added to the functions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -28,18 +28,79 @@ Object.keys(defaultEnv).forEach(
 console.log(contentToWrite);
 
 // All the file handling operation will be performed using fs
-const fs = require("node:fs");
+// const fs = require("node:fs");
+
 
 // Synchronous way
 // fs.writeFileSync(".env", contentToWrite);
 
 // Asynchronous way
 // fs.writeFile(filepath, filecontents, callback)
-fs.writeFile(".env", contentToWrite, (error) => {
-    if (error) {
+
+// console.log("Before the fs callback");
+// fs.writeFile(".env", contentToWrite, (error) => {
+//     if (error) {
+//         console.log("Error encountered: ", error.message);
+//     } else {
+//         console.log("File written successfully");
+//     }
+// });
+
+// console.log("After the fs callback");
+
+// console.log("File created successfully");
+
+
+// Promise version of node-fs
+const fs = require("node:fs/promises");
+
+// console.log("Before the fs promise");
+// fs.writeFile(".env", contentToWrite)
+// .then(() => {
+//     console.log("After the file has been created");
+//     console.log("Extra operations");
+//     throw new Error("Some error occured");
+// // }).then(() => {
+// //     fs.writeFile(".env", contentToWrite)
+// //     .then(() => {
+// //         console.log("After the file has been written in the 2nd file");
+
+// //         fs.writeFile(".env", contentToWrite)
+// //         .then(() => {
+// //             console.log("Written in the 3rd file");
+// //         }).catch((error) => {
+// //             console.log("Error encountered in the 3rd file: ", error.message);
+// //         });
+// //     }).catch((error) => {
+// //         console.log("Error encountered in the 2nd file: ", error.message);
+// //     });
+// }).catch((error) => {
+//     console.log("Error encountered: ", error.message);
+
+// }).finally(() => {
+//     console.log("All File operations created successfully");
+// });
+// console.log("After the fs promise");
+
+async function writeEnvFile() {
+    console.log("Before the await");
+    try {
+        let result = await fs.writeFile(".env", contentToWrite);
+        console.log("Await process executed here...");
+    } catch (error) {
         console.log("Error encountered: ", error.message);
-    } else {
-        console.log("File written successfully");
     }
-})
-console.log("File created successfully");
+    console.log("After the await");
+};
+
+writeEnvFile();
+
+async function app() {
+    console.log("Before calling the app function");
+    await writeEnvFile().then(() => {
+        console.log("Did this work? Let's find out");
+    });
+    console.log("After calling the app function");
+}
+
+app();
